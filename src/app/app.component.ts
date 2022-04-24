b7538b8a0e607b6f8adbfd3b0b677ed3235e3292
prajjwal2411 <prajjwal1997@gmail.com>
2022-04-24 22:52:15 +0530

    trying-map-fumctions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'project-india';
+
+  options: google.maps.MapOptions = {
+    center: { lat: 20.5937, lng: 78.9629 },
+    zoom: 5,
+  }
+
+
+  ngOnInit(): void {}
+
 }
